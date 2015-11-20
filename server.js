652f7d5c4c28886a9beef2645b606a5a91803d36
Ilya Kharlamov <ilya.kharlamov@gmail.com>
2015-11-21 01:10:40 +0300

    initial investigation

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
+var geoutil = require('./lib/geoutil');
 var app = express();
 app.use("/static", express.static(__dirname + '/static'));
 app.get("/", function (req, res, next) {
 	res.redirect("/static");
 });
-app.get('/api/1/map-version/3/:z/:x/:y', function (req, res, next) {
+var dbconfig = require('./db.config');
+app.get('/api/1/map-version/3/vector/:z/:x/:y', function (req, res, next) {
 	console.log(req.params);
 
     try {
@@ -12,39 +14,19 @@ app.get('/api/1/map-version/3/:z/:x/:y', function (req, res, next) {
       var z = parseInt(req.params.z);
       var x = parseInt(req.params.x);
       var y = parseInt(req.params.y);
-
-      var key = z + '-' + x + '-' + y;
-
-      var c = cache.get(key);
-
-      if (c) {
-        res.send(c);
-        return;
-      }
-
-      var box = util.getLTRBbox(z, x, y);
-
-      db.getTileData(box.left, box.top, box.right, box.bottom)
-        .then(function (data) {
-
-          return parseResult(data);
-
-        })
-        .then(function (featureCollection) {
-
-          cache.set(key, featureCollection);
-          res.send(featureCollection);
-
-        })
-        .catch(function (err) {
-          logger.error(err);
-          next(err);
-        });
-
+      console.log("dbconfig", dbconfig);
+      res.json({
+        "fu":"ck",
+        "z":z,
+        "x":x,
+        "y":y,
+        "long": geoutil.tile2long(x,z),
+        "lat": geoutil.tile2lat(y,z)
+      });
 
     } catch (err) {
 
-      logger.error(err);
+      console.log(err);
       next(err);
     }
 
